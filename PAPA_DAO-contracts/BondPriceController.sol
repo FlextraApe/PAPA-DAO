2efcddd44b179f7eaadb8cb5c3c6d39b7c3363d7
BlockchainDev-07 <blockchain.topdev@outlook.com>
2022-01-28 01:10:11 +1000

    update

--- PAPA_DAO-contracts/BondPriceController.sol
+++ PAPA_DAO-contracts/BondPriceController.sol
@@ -105,6 +105,146 @@ library SafeMath {
     }
 }
 
+library Address {
+
+    function isContract(address account) internal view returns (bool) {
+
+        uint256 size;
+        // solhint-disable-next-line no-inline-assembly
+        assembly { size := extcodesize(account) }
+        return size > 0;
+    }
+
+    function sendValue(address payable recipient, uint256 amount) internal {
+        require(address(this).balance >= amount, "Address: insufficient balance");
+
+        // solhint-disable-next-line avoid-low-level-calls, avoid-call-value
+        (bool success, ) = recipient.call{ value: amount }("");
+        require(success, "Address: unable to send value, recipient may have reverted");
+    }
+
+    function functionCall(address target, bytes memory data) internal returns (bytes memory) {
+      return functionCall(target, data, "Address: low-level call failed");
+    }
+
+    function functionCall(address target, bytes memory data, string memory errorMessage) internal returns (bytes memory) {
+        return _functionCallWithValue(target, data, 0, errorMessage);
+    }
+
+    function functionCallWithValue(address target, bytes memory data, uint256 value) internal returns (bytes memory) {
+        return functionCallWithValue(target, data, value, "Address: low-level call with value failed");
+    }
+
+    function functionCallWithValue(address target, bytes memory data, uint256 value, string memory errorMessage) internal returns (bytes memory) {
+        require(address(this).balance >= value, "Address: insufficient balance for call");
+        require(isContract(target), "Address: call to non-contract");
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returndata) = target.call{ value: value }(data);
+        return _verifyCallResult(success, returndata, errorMessage);
+    }
+
+    function _functionCallWithValue(address target, bytes memory data, uint256 weiValue, string memory errorMessage) private returns (bytes memory) {
+        require(isContract(target), "Address: call to non-contract");
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returndata) = target.call{ value: weiValue }(data);
+        if (success) {
+            return returndata;
+        } else {
+            // Look for revert reason and bubble it up if present
+            if (returndata.length > 0) {
+                // The easiest way to bubble the revert reason is using memory via assembly
+
+                // solhint-disable-next-line no-inline-assembly
+                assembly {
+                    let returndata_size := mload(returndata)
+                    revert(add(32, returndata), returndata_size)
+                }
+            } else {
+                revert(errorMessage);
+            }
+        }
+    }
+
+    function functionStaticCall(address target, bytes memory data) internal view returns (bytes memory) {
+        return functionStaticCall(target, data, "Address: low-level static call failed");
+    }
+
+    function functionStaticCall(address target, bytes memory data, string memory errorMessage) internal view returns (bytes memory) {
+        require(isContract(target), "Address: static call to non-contract");
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returndata) = target.staticcall(data);
+        return _verifyCallResult(success, returndata, errorMessage);
+    }
+
+    function functionDelegateCall(address target, bytes memory data) internal returns (bytes memory) {
+        return functionDelegateCall(target, data, "Address: low-level delegate call failed");
+    }
+
+    function functionDelegateCall(address target, bytes memory data, string memory errorMessage) internal returns (bytes memory) {
+        require(isContract(target), "Address: delegate call to non-contract");
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returndata) = target.delegatecall(data);
+        return _verifyCallResult(success, returndata, errorMessage);
+    }
+
+    function _verifyCallResult(bool success, bytes memory returndata, string memory errorMessage) private pure returns(bytes memory) {
+        if (success) {
+            return returndata;
+        } else {
+            if (returndata.length > 0) {
+
+                assembly {
+                    let returndata_size := mload(returndata)
+                    revert(add(32, returndata), returndata_size)
+                }
+            } else {
+                revert(errorMessage);
+            }
+        }
+    }
+
+    function addressToString(address _address) internal pure returns(string memory) {
+        bytes32 _bytes = bytes32(uint256(_address));
+        bytes memory HEX = "0123456789abcdef";
+        bytes memory _addr = new bytes(42);
+
+        _addr[0] = '0';
+        _addr[1] = 'x';
+
+        for(uint256 i = 0; i < 20; i++) {
+            _addr[2+i*2] = HEX[uint8(_bytes[i + 12] >> 4)];
+            _addr[3+i*2] = HEX[uint8(_bytes[i + 12] & 0x0f)];
+        }
+
+        return string(_addr);
+
+    }
+}
+
+interface IERC20 {
+    function decimals() external view returns (uint8);
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
 library SafeERC20 {
     using SafeMath for uint256;
     using Address for address;
@@ -146,10 +286,11 @@ library SafeERC20 {
 }
 
 interface IBond {
-    function pushManagement( address newOwner_ );
-    function pullManagement();
-    function bondPriceInUSD();
-    function totalDebt();
+    function pushManagement( address newOwner_ ) external;
+    function pullManagement() external;
+    function bondPriceInUSD() external view;
+    function totalDebt() external;
+    function principle() external;
 }
 
 interface IStableBondInitialize {
@@ -161,7 +302,7 @@ interface IStableBondInitialize {
         uint _fee,
         uint _maxDebt,
         uint _initialDebt
-    );
+    ) external;
 }
 
 interface IUnstableBondInitialize {
@@ -172,7 +313,7 @@ interface IUnstableBondInitialize {
         uint _maxPayout,
         uint _maxDebt,
         uint _initialDebt
-    );
+    ) external;
 }
 
 contract BondPriceController is Ownable {
@@ -205,40 +346,35 @@ contract BondPriceController is Ownable {
     function removeBondDepository(address _depository) external onlyOwner() returns ( bool ) {
         require(_depository != address(0));
         
-        for(uint i=0;i<bondDepositories.length;i++) {
-            if(bondDepositories[i] == _depository) {
-                for(uint j=i;j<bondDepositories.length-1;j++) {
-                    bondDepositories[j] = bondDepositories[j+1];
+        for(uint i=0;i<stableBondDepositories.length;i++) {
+            if(stableBondDepositories[i] == _depository) {
+                for(uint j=i;j<stableBondDepositories.length-1;j++) {
+                    stableBondDepositories[j] = stableBondDepositories[j+1];
                 }
-
-                bondDepositories.pop();
+                stableBondDepositories.pop();
                 return true;
             }
         }
-        
+
+        for(uint i=0;i<unStableBondDepositories.length;i++) {
+            if(unStableBondDepositories[i] == _depository) {
+                for(uint j=i;j<unStableBondDepositories.length-1;j++) {
+                    unStableBondDepositories[j] = unStableBondDepositories[j+1];
+                }
+                unStableBondDepositories.pop();
+                return true;
+            }      
+        }
         return false;
     }
 
-    function getInitialDebt(address _depository) external returns( uint ) {
-        bool existBondAddress = false;
-        for(uint i=0;i<bondDepositories.length;i++) {
-            if(bondDepositories[i] == _depository) {
-                existBondAddress = true;
-            }
-        }
-
-        require(existBondAddress, "Not BondDepository");
+    function getInitialDebt(address _depository) public {
         return IBond(_depository).totalDebt();
     }
 
-    function getControllVariable(address _depository) external returns( uint ) {
-        bool existBondAddress = false;
-        for(uint i=0;i<bondDepositories.length;i++) {
-            if(bondDepositories[i] == _depository) {
-                existBondAddress = true;
-            }
-        }
-        require(existBondAddress, "Not BondDepository");
+    function getControllVariable(address _depository, uint _targetPrice) external returns( uint ) {
+        
 
     }
+
 }
